8e5b601c32ebe62b21c58afe878fb8e3d94ec7fa
Paul-Kijtapart <scc385@gmail.com>
2017-03-24 22:29:38 -0700

    Skill bar chart is working atm

--- src/js/components/BarChartView.js
+++ src/js/components/BarChartView.js
@@ -6,33 +6,93 @@ import {
 	CartesianGrid,
 	Tooltip,
 	Legend,
-	Bar
+	Bar,
+	ResponsiveContainer
 } from 'recharts';
 
-// const skill_bar_chart_config = {
-// 	width: 600,
-// 	height:300,
-// 	margin: {}
-// };
+
+const CustomAxisTick = function(props) {
+	var getMatchingIcon = function(level) {
+		switch (level) {
+			case 2:
+				break;
+			case 4:
+				break;
+			case 6:
+				break;
+			case 8:
+				break;
+			case 10:
+				break;
+		}
+		return undefined;
+	};
+
+	var toProficientString = function(level, level_meaning) {
+		if (!(level in level_meaning)) {
+			return "";
+		}
+		return {
+			icon: getMatchingIcon(level),
+			meaning: level_meaning[level]
+		}
+	};
+	const {
+		level_meaning,
+		fill,
+		x,
+		y,
+		stroke,
+		payload
+	} = props;
+
+	console.log(level_meaning);
+	console.log(payload.value);
+
+	let res = toProficientString(payload.value, level_meaning)
+	return (
+		<g>
+			<text x={x} y={y} dy={34} textAnchor="middle" fill={fill}>
+    			{res.icon}
+    			{res.meaning}
+    		</text>
+		</g>
+	);
+};
 
 class BarChartView extends React.Component {
 	render() {
 		const technical_skill_list = this.props.technical_skill_list;
+		const level_meaning = this.props.level_meaning;
 		return (
-			<div className="barChartView">
-				<p> Hi! i am barChartView. </p>
-				<BarChart 
-					width={600} 
-					height={300} 
-					data={technical_skill_list}
-           	 		margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-					<XAxis dataKey="name"/>
-					<YAxis/>
-					<CartesianGrid strokeDasharray="3 3"/>
-					<Tooltip/>
-					<Legend />
-					<Bar dataKey="level" fill="#8884d8" />
-				</BarChart>
+			<div className="barChartView"> 
+				<ResponsiveContainer 
+					width={"90%"}
+					height={500}
+				>
+					<BarChart 
+					  data={technical_skill_list} 
+					  layout="vertical"
+					  margin={{top: 5, right: 30, left: 20, bottom: 15}}
+					>
+					  <XAxis 
+					  	domain={[0, 10]} 
+					  	stroke="white" 
+					  	type="number"
+					  	interval={0}
+					  	tickCount={6}
+					  	tick={<CustomAxisTick level_meaning={level_meaning}/>}
+					  />
+					  <YAxis 
+					  	stroke="white" 
+					  	type="category" 
+					  	dataKey="name" 
+					  />
+					  <CartesianGrid strokeDasharray="3 3"/>
+					  <Tooltip/>
+					  <Bar dataKey="level" fill="#82ca9d" />
+					</BarChart>
+				</ResponsiveContainer>
 			</div>
 		);
 	}
--- src/js/components/Skill.js
+++ src/js/components/Skill.js
@@ -6,11 +6,15 @@ class Skill extends React.Component {
 		const aorInfo = this.props.aorInfo;
 		const technical_skill_list =
 			aorInfo.technical_skills;
+		const level_meaning =
+			aorInfo.level_meaning;
+
 		return (
 			<div className="skill">
 			<h2> Skill </h2>
 			<BarChartView 
 				technical_skill_list={technical_skill_list}
+				level_meaning={level_meaning}
 			/>
 		</div>
 		);
